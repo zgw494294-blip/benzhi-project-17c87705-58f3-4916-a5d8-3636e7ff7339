dc78e9d7b99592208c985ef46d43bb81a3e6d6e0
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 17c87705-58f3-4916-a5d8-3636e7ff7339-bug-1

--- internal/application/cases.go
+++ internal/application/cases.go
@@ -2,7 +2,9 @@ package application
 
 import (
 	"context"
+	"encoding/json"
 	"oralarchive/internal/domain"
+	"oralarchive/internal/storage"
 	"strings"
 )
 
@@ -18,14 +20,34 @@ func (s *Service) CreateCase(ctx context.Context, cmd CreateCaseCommand) (domain
 	if collector == "" {
 		collector = p.Name
 	}
-	c, err := domain.NewCase(newID("case"), cmd.Title, cmd.IntervieweeAlias, collector, s.now())
+	var result domain.OralHistoryCase
+	err = s.store.WithTx(ctx, func(tx *storage.Tx) error {
+		if cached, ok, lookupErr := tx.GetIdempotencyByKey(ctx, cmd.IdempotencyKey, "case"); lookupErr != nil {
+			return lookupErr
+		} else if ok {
+			return json.Unmarshal(cached, &result)
+		}
+		c, newErr := domain.NewCase(newID("case"), cmd.Title, cmd.IntervieweeAlias, collector, s.now())
+		if newErr != nil {
+			return newErr
+		}
+		if err = tx.InsertCase(ctx, *c); err != nil {
+			return err
+		}
+		if err = tx.AppendAudit(ctx, domain.AuditEvent{CaseID: c.CaseID, Action: "CASE_CREATED", Actor: p.Name, Detail: "建立口述史采集案卷", Version: c.Version, OccurredAt: c.CreatedAt}); err != nil {
+			return err
+		}
+		result = *c
+		response, marshalErr := json.Marshal(result)
+		if marshalErr != nil {
+			return marshalErr
+		}
+		return tx.PutIdempotency(ctx, cmd.IdempotencyKey, "case", c.CaseID, response)
+	})
 	if err != nil {
-		return domain.OralHistoryCase{}, err
-	}
-	if err = s.store.CreateCase(ctx, *c, p.Name); err != nil {
 		return domain.OralHistoryCase{}, mapStorage(err)
 	}
-	return *c, nil
+	return result, nil
 }
 func (s *Service) GetCase(ctx context.Context, id string) (domain.OralHistoryCase, error) {
 	c, err := s.store.GetCase(ctx, id)
--- internal/storage/idempotency.go
+++ internal/storage/idempotency.go
@@ -26,3 +26,19 @@ func (tx *Tx) PutIdempotency(ctx context.Context, key, operation, caseID string,
 	_, err := tx.tx.ExecContext(ctx, `INSERT INTO idempotency_keys(key,operation,case_id,response,created_at) VALUES(?,?,?,?,?)`, key, operation, caseID, response, time.Now().UTC().Format(time.RFC3339Nano))
 	return err
 }
+func (s *Store) GetIdempotencyByKey(ctx context.Context, key, operation string) ([]byte, bool, error) {
+	var response []byte
+	err := s.db.QueryRowContext(ctx, `SELECT response FROM idempotency_keys WHERE key=? AND operation=?`, key, operation).Scan(&response)
+	if err == sql.ErrNoRows {
+		return nil, false, nil
+	}
+	return response, err == nil, err
+}
+func (tx *Tx) GetIdempotencyByKey(ctx context.Context, key, operation string) ([]byte, bool, error) {
+	var response []byte
+	err := tx.tx.QueryRowContext(ctx, `SELECT response FROM idempotency_keys WHERE key=? AND operation=?`, key, operation).Scan(&response)
+	if err == sql.ErrNoRows {
+		return nil, false, nil
+	}
+	return response, err == nil, err
+}
